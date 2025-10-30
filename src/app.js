15a31ab885d21bf3e93b2b58c5c282dee2fd0837
umarfarooq897 <ufofficial.cs@gmail.com>
2025-10-30 13:13:59 +0500

    Route learning

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello  world!");
+app.get("/user", (req, res) => {
+  console.log(req.body);
+  res.send("data Save Successfully");
 });
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
